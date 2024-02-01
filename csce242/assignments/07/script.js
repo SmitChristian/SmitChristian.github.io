247d1fff2e58ff1d34e2dace65f736d22a52ae15
smitChristian <crs29@email.sc.edu>
2024-01-31 20:25:46 -0500

    debugging

--- csce242/assignments/07/script.js
+++ csce242/assignments/07/script.js
@@ -2,17 +2,18 @@ window.onload = () => {
 
     // Image Swapping
     document.getElementById("image-click").onclick = () => {
+        alert(document.getElementById("image-click").src);
         if (document.getElementById("image-click").src == "https://smitchristian.github.io/csce242/assignments/07/images/radiation.jpg"){
-            document.getElementById("image-click").src = "https://smitchristian.github.io/csce242/assignments/07/images/Psyche.png";
+            document.getElementById("image-click").src = "https://smitchristian.github.io/csce242/assignments/07/images/no-air.jpg";
             document.getElementById("words-click").innerHTML = "The most immediate threat in the cosmic vacuum is oxygen deprivation. Assuming that you dont hold your breath during decompression, it will take about 15 seconds for your O2 deprived blood to get to your brain. When this happens, youll pass out…and then youll die.";
             document.getElementById("image-click-header").innerHTML = "Asphyxiation!"
-        } else if (document.getElementById("image-click").src == "https://SmitChristian.github.io/csce242/assignments/07/images/swarm2.jpg"){
-            document.getElementById("image-click").src = "https://SmitChristian.github.io/csce242/assignments/07/images/Psyche.png";
+        } else if (document.getElementById("image-click").src == "https://smitchristian.github.io/csce242/assignments/07/images/no-air.jpg"){
+            document.getElementById("image-click").src = "https:/smitchristian.github.io/csce242/assignments/07/images/Psyche.png";
             document.getElementById("words-click").innerHTML = "After about 10 seconds, the moisture in your body will start to evaporate. This is known as ebullism and it happens because the reduction in pressure causes the boiling point of your bodily fluids to decrease. And if evaporating body fluids doesnt sound terribly pleasant, well, its because its not.";
             document.getElementById("image-click-header").innerHTML = "Evaporation!"
         }
         else {
-            document.getElementById("image-click").src = "https://SmitChristian.github.io/csce242/assignments/07/images/radiation.jpg";
+            document.getElementById("image-click").src = "https://smitchristian.github.io/csce242/assignments/07/images/radiation.jpg";
             document.getElementById("words-click").innerHTML = "Radiation can alter the cardiovascular system, damaging the heart, harden and narrow arteries, and/or eliminate some of the cells in linings of the blood vessels, leading to cardiovascular disease.";
             document.getElementById("image-click-header").innerHTML = "Radiation!"
         }
