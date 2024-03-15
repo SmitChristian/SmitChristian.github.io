c2d9fbb9e31978e8a4ab629e434f258c3e00334d
smitChristian <crs29@email.sc.edu>
2024-03-15 15:54:30 -0400

    adding projects part 5 and 6

--- projects/part5/conceptualdesign/risk/script.js
+++ projects/part5/conceptualdesign/risk/script.js
@@ -8,12 +8,69 @@ function myFunction() {
     }
   }
 
-async function getData {
-  data = url()
+async function getData() {
+  const url = "https://SmitChristian.github.io/projects/part5/conceptualdesign/risk/riskdata.json";
+
+
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+async function showData() {
+  let data = await getData();
+  let toAddTo = document.getElementById("sec-main");
+
+  data.forEach((dataPoint) => {
+    toAddTo.append(processData(dataPoint));
+  })
+}
+
+function processData(point) {
+  let output = document.createElement("section");
+
+  const dataName = document.createElement("h2");
+  dataName.innerText = point.name;
+  output.append(dataName);
+
+  let riskMap = document.createElement("img");
+  riskMap.src = point.map;
+  output.append(riskMap);
+
+  let riskTab = document.createElement("img");
+  riskTab.src = point.tab;
+  output.append(riskTab);
+
+  let listData = document.createElement("ul");
+  listData.append(getLi(`CPU: ${point.config_info[0].cpu}`));
+  listData.append(getLi(`POWER: ${point.config_info[0].power}`));
+  listData.append(getLi(`CONTROL: ${point.config_info[0].control}`));
+  output.append(listData);
+
+  let dataPage1 = document.createElement('p');
+  dataPage1.innerHTML = point.config_info[1].data_page1;
+  output.append(dataPage1);
+
+  let dataPage2 = document.createElement('p');
+  dataPage2.innerHTML = point.config_info[1].data_page2;
+  output.append(dataPage2);
+
+  
+  return output;
+}
+
+function getLi (data) {
+  const li = document.createElement("li");
+  li.textContent = data;
+  li.classList.add("links");
+
+  return li;
 }
 
 
-
 window.onload = () => {
-
+  showData();
 }
